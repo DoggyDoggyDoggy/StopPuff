04751e28e2ad00afa3ed4ec8226931b9d6a3fc97
Denys <pedan.denys@gmail.com>
2025-05-02 15:00:49 +1200

    OnBoardingScreen Slides

--- app/src/main/java/denys/diomaxius/stoppuff/ui/screen/onboarding/OnBoardingScreen.kt
+++ app/src/main/java/denys/diomaxius/stoppuff/ui/screen/onboarding/OnBoardingScreen.kt
@@ -3,26 +3,43 @@
 package denys.diomaxius.stoppuff.ui.screen.onboarding
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import denys.diomaxius.stoppuff.R
+import denys.diomaxius.stoppuff.navigation.Screen
 
 
 @Composable
 fun OnBoardingScreen(
     navHostController: NavHostController
 ) {
-    val pagerState = rememberPagerState { 2 }
     val slides = listOf<@Composable () -> Unit>(
         { FirstSlide() },
-        { SecondSlide() }
+        { SecondSlide() },
+        { ThirdSlide(navHostController = navHostController) }
     )
+
+    val pagerState = rememberPagerState { slides.size }
 
     Content(
         pagerState = pagerState,
@@ -49,12 +66,112 @@ fun Content(
 
 @Composable
 fun FirstSlide() {
-    Text(text = "First Slide")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 18.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = stringResource(R.string.onboarding_slide_one_name),
+            fontWeight = FontWeight.Medium,
+            fontSize = 26.sp
+        )
+
+        Text(
+            text = stringResource(R.string.onboarding_slide_one_description),
+            textAlign = TextAlign.Center,
+            fontSize = 16.sp
+        )
+    }
 }
 
 @Composable
 fun SecondSlide() {
-    Text(text = "Second Slide")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 18.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = stringResource(R.string.onboarding_slide_two_name),
+            fontWeight = FontWeight.Medium,
+            fontSize = 26.sp
+        )
+
+        Text(
+            text = stringResource(R.string.onboarding_slide_two_description),
+            textAlign = TextAlign.Center,
+            fontSize = 16.sp
+        )
+    }
+}
+
+@Composable
+fun ThirdSlide(
+    navHostController: NavHostController
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Spacer(modifier = Modifier.height(1.dp)) // Third child
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Last puffs",
+                fontWeight = FontWeight.Medium,
+                fontSize = 26.sp
+            )
+
+            Text(
+                text = "Vape last time before you start",
+                textAlign = TextAlign.Center,
+                fontSize = 16.sp
+            )
+        }
+
+        Button(
+            modifier = Modifier.padding(bottom = 32.dp),
+            onClick = {
+                navHostController.navigate(Screen.Main.route) {
+                    popUpTo(Screen.Onboarding.route) { inclusive = true }
+                    launchSingleTop = true
+                }
+            }
+        ) {
+            Text(text = "Start")
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun FirstSlidePreview() {
+    FirstSlide()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SecondSlidePreview() {
+    SecondSlide()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ThirdSlidePreview() {
+    ThirdSlide(
+        navHostController = rememberNavController()
+    )
 }
 
 /*
