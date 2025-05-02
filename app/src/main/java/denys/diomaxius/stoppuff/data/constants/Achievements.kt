9a249afdb127a0debd9a98633874ed812efaf076
Denys <pedan.denys@gmail.com>
2025-05-03 08:43:59 +1200

    Achievements separate file

---
+++ app/src/main/java/denys/diomaxius/stoppuff/data/constants/Achievements.kt
@@ -0,0 +1,9 @@
+package denys.diomaxius.stoppuff.data.constants
+
+object Achievements {
+    val achievements = listOf<String>(
+        "1 Day Puff Free",
+        "3 Days Puff Free",
+        "7 Days Puff Free"
+    )
+}
