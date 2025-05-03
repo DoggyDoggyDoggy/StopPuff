65306f12adb045885892fb5b29cf5db8dd9e9177
Denys <pedan.denys@gmail.com>
2025-05-03 19:44:47 +1200

    DI AppLaunchRepository

--- app/src/main/java/denys/diomaxius/stoppuff/di/AppModule.kt
+++ app/src/main/java/denys/diomaxius/stoppuff/di/AppModule.kt
@@ -6,8 +6,11 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import denys.diomaxius.stoppuff.data.datastore.AppLaunchPreferences
 import denys.diomaxius.stoppuff.data.datastore.QuitPreferences
+import denys.diomaxius.stoppuff.data.repository.AppLaunchRepositoryImpl
 import denys.diomaxius.stoppuff.data.repository.QuitDateRepositoryImpl
+import denys.diomaxius.stoppuff.domain.repository.AppLaunchRepository
 import denys.diomaxius.stoppuff.domain.repository.QuitDateRepository
 import javax.inject.Singleton
 
@@ -21,6 +24,16 @@ object AppModule {
 
     @Provides
     @Singleton
+    fun provideAppLaunchPreferences(@ApplicationContext context: Context): AppLaunchPreferences =
+        AppLaunchPreferences(context)
+
+    @Provides
+    @Singleton
     fun provideQuitDateRepository(prefs: QuitPreferences): QuitDateRepository =
         QuitDateRepositoryImpl(prefs)
+
+    @Provides
+    @Singleton
+    fun provideAppLaunchRepository(prefs: AppLaunchPreferences): AppLaunchRepository =
+        AppLaunchRepositoryImpl(prefs)
 }
