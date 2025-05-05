5235442f1d6c128e7af1acbc6316ab45a2f77f96
Denys <pedan.denys@gmail.com>
2025-05-05 22:13:08 +1200

    Extra achievements, new UI

--- app/src/main/java/denys/diomaxius/stoppuff/data/constants/Achievements.kt
+++ app/src/main/java/denys/diomaxius/stoppuff/data/constants/Achievements.kt
@@ -10,6 +10,13 @@ object Achievements {
         Achievement("4 Days Puff Free", 4),
         Achievement("5 Days Puff Free", 5),
         Achievement("6 Days Puff Free", 6),
-        Achievement("7 Days Puff Free", 7)
+        Achievement("7 Days Puff Free", 7),
+        Achievement("2 Weeks Puff Free", 14),
+        Achievement("3 Weeks Puff Free", 21),
+        Achievement("4 Weeks Puff Free", 28),
+        Achievement("1 Month Puff Free", 30),
+        Achievement("2 Months Puff Free", 60),
+        Achievement("3 Months Puff Free", 90),
+
     )
 }
--- app/src/main/java/denys/diomaxius/stoppuff/ui/screen/main/Achievement.kt
+++ app/src/main/java/denys/diomaxius/stoppuff/ui/screen/main/Achievement.kt
@@ -1,7 +1,6 @@
 package denys.diomaxius.stoppuff.ui.screen.main
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -18,6 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -32,28 +32,25 @@ fun Achievement(
             .padding(vertical = 8.dp),
         elevation = CardDefaults.cardElevation(6.dp)
     ) {
-        Box(
+        Row(
             modifier = Modifier.fillMaxWidth(),
-            contentAlignment = Alignment.Center
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Icon(
-                    modifier = Modifier
-                        .padding(12.dp)
-                        .size(64.dp)
-                        .clip(CircleShape)
-                        .background(if (achieved) Color.Green else Color.Gray),
-                    imageVector = Icons.Default.Check,
-                    tint = if (achieved) Color.White else Color.DarkGray,
-                    contentDescription = "Check"
-                )
-            }
+            Icon(
+                modifier = Modifier
+                    .padding(vertical = 12.dp).padding(start = 12.dp)
+                    .size(64.dp)
+                    .clip(CircleShape)
+                    .background(if (achieved) Color.Green else Color.Gray),
+                imageVector = Icons.Default.Check,
+                tint = if (achieved) Color.White else Color.DarkGray,
+                contentDescription = "Check"
+            )
 
             Text(
-                modifier = Modifier.padding(start = 22.dp),
+                modifier = Modifier
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Center,
                 text = text,
                 fontSize = 28.sp,
             )
@@ -65,7 +62,7 @@ fun Achievement(
 @Composable
 fun AchievementPreview() {
     Achievement(
-        text = "aasdasdasdasdasd",
+        text = "1 Day Puff Free",
         achieved = true
     )
 }
