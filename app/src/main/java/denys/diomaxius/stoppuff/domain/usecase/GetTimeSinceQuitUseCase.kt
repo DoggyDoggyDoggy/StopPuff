c170fddd30cabc8d7d32e4f4b7f23835ab19b09a
Denys <pedan.denys@gmail.com>
2025-05-05 14:03:14 +1200

    GetTimeSinceQuitUseCase

---
+++ app/src/main/java/denys/diomaxius/stoppuff/domain/usecase/GetTimeSinceQuitUseCase.kt
@@ -0,0 +1,17 @@
+package denys.diomaxius.stoppuff.domain.usecase
+
+import java.time.Duration
+import java.time.LocalDateTime
+
+class GetTimeSinceQuitUseCase {
+    operator fun invoke(quitDate: LocalDateTime?): Triple<Long, Long, Long> {
+        return quitDate?.let {
+            val duration = Duration.between(it, LocalDateTime.now())
+            val days = duration.toDays()
+            val hours = duration.minusDays(days).toHours()
+            val minutes = duration.minusDays(days).minusHours(hours).toMinutes()
+
+            Triple(days, hours, minutes)
+        } ?: Triple(0, 0, 0)
+    }
+}
--- app/src/main/java/denys/diomaxius/stoppuff/ui/screen/main/MainScreenViewModel.kt
+++ app/src/main/java/denys/diomaxius/stoppuff/ui/screen/main/MainScreenViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import denys.diomaxius.stoppuff.domain.usecase.GetQuitDateUseCase
+import denys.diomaxius.stoppuff.domain.usecase.GetTimeSinceQuitUseCase
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
@@ -14,13 +15,12 @@ import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
-import java.time.Duration
-import java.time.LocalDateTime
 import javax.inject.Inject
 
 @HiltViewModel
 class MainScreenViewModel @Inject constructor(
-    getQuitDateUseCase: GetQuitDateUseCase
+    getQuitDateUseCase: GetQuitDateUseCase,
+    private val getTimeSinceQuit: GetTimeSinceQuitUseCase
 ) : ViewModel() {
     private val _quitDate = getQuitDateUseCase()
         .stateIn(viewModelScope, SharingStarted.Lazily, null)
@@ -50,16 +50,5 @@ class MainScreenViewModel @Inject constructor(
             delay(period)
             emit(Unit)
         }
-    }
-
-    private fun getTimeSinceQuit(quitDate: LocalDateTime?): Triple<Long, Long, Long> {
-        return quitDate?.let {
-            val duration = Duration.between(it, LocalDateTime.now())
-            val days = duration.toDays()
-            val hours = duration.minusDays(days).toHours()
-            val minutes = duration.minusDays(days).minusHours(hours).toMinutes()
-
-            Triple(days, hours, minutes)
-        } ?: Triple(0, 0, 0)
     }
 }
