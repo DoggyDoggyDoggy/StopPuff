995a16cccf88857b05efddd483109e9630721514
Denys <pedan.denys@gmail.com>
2025-05-09 15:17:37 +1200

    TextField save daily spending

--- app/src/main/java/denys/diomaxius/stoppuff/ui/screen/onboarding/OnBoardingScreenViewModel.kt
+++ app/src/main/java/denys/diomaxius/stoppuff/ui/screen/onboarding/OnBoardingScreenViewModel.kt
@@ -3,7 +3,9 @@ package denys.diomaxius.stoppuff.ui.screen.onboarding
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import denys.diomaxius.stoppuff.domain.usecase.GetDailySpendingUseCase
 import denys.diomaxius.stoppuff.domain.usecase.GetFirstLaunchUseCase
+import denys.diomaxius.stoppuff.domain.usecase.SaveDailySpendingUseCase
 import denys.diomaxius.stoppuff.domain.usecase.SaveQuitDateUseCase
 import denys.diomaxius.stoppuff.domain.usecase.SetFirstLaunchUseCase
 import kotlinx.coroutines.flow.SharingStarted
@@ -17,12 +19,30 @@ import javax.inject.Inject
 class OnBoardingScreenViewModel @Inject constructor(
     private val saveQuitDateUseCase: SaveQuitDateUseCase,
     private val setFirstLaunchUseCase: SetFirstLaunchUseCase,
-    getFirstLaunchUseCase: GetFirstLaunchUseCase
+    getFirstLaunchUseCase: GetFirstLaunchUseCase,
+    getDailySpendingUseCase: GetDailySpendingUseCase,
+    private val saveDailySpendingUseCase: SaveDailySpendingUseCase
 ) : ViewModel() {
 
     private val _firstLaunch = getFirstLaunchUseCase()
         .stateIn(viewModelScope, SharingStarted.Lazily, null)
     val firstLaunch: StateFlow<Boolean?> = _firstLaunch
+
+    private val _dailySpend = getDailySpendingUseCase()
+        .stateIn(viewModelScope, SharingStarted.Lazily, null)
+    val dailySpend: StateFlow<Int?> = _dailySpend
+
+    fun saveDailySpending(str: String) {
+        viewModelScope.launch {
+            var amount = 0
+            if (str.length < 5) {
+                if (str.all { it.isDigit() } && str.isNotEmpty()) {
+                    amount = str.toInt()
+                }
+            }
+            saveDailySpendingUseCase(amount)
+        }
+    }
 
     fun saveLastDatePuff(dateTime: LocalDateTime = LocalDateTime.now()) {
         viewModelScope.launch {
--- app/src/main/java/denys/diomaxius/stoppuff/ui/screen/onboarding/slide/ThirdSlide.kt
+++ app/src/main/java/denys/diomaxius/stoppuff/ui/screen/onboarding/slide/ThirdSlide.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,6 +15,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -21,7 +24,10 @@ import androidx.compose.ui.unit.sp
 import denys.diomaxius.stoppuff.R
 
 @Composable
-fun ThirdSlide() {
+fun ThirdSlide(
+    dailySpend: Int?,
+    saveDailySpending: (String) -> Unit
+) {
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -46,10 +52,14 @@ fun ThirdSlide() {
         Spacer(modifier = Modifier.height(24.dp))
         OutlinedTextField(
             modifier = Modifier.fillMaxWidth(),
-            value = "",
-            onValueChange = {},
-            label = { Text(text = "Enter the amount")},
-            placeholder = { Text(text = "For example 10")}
+            value = "${dailySpend ?: 0}",
+            onValueChange = { saveDailySpending(it) },
+            label = { Text(text = "Enter the amount") },
+            placeholder = { Text(text = "For example 10") },
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            )
         )
 
         Spacer(modifier = Modifier.height(12.dp))
@@ -64,5 +74,8 @@ fun ThirdSlide() {
 @Preview(showBackground = true)
 @Composable
 fun ThirdSlidePreview() {
-    ThirdSlide()
+    ThirdSlide(
+        dailySpend = 0,
+        saveDailySpending = {}
+    )
 }
