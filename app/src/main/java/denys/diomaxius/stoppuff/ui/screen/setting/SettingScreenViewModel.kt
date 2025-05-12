0ec46faaf6a4e693556b1918c4fff515e6c7c241
Denys <pedan.denys@gmail.com>
2025-05-12 19:53:49 +1200

    SettingScreenViewModel.kt

---
+++ app/src/main/java/denys/diomaxius/stoppuff/ui/screen/setting/SettingScreenViewModel.kt
@@ -0,0 +1,34 @@
+package denys.diomaxius.stoppuff.ui.screen.setting
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import denys.diomaxius.stoppuff.domain.usecase.GetDailySpendingUseCase
+import denys.diomaxius.stoppuff.domain.usecase.SaveDailySpendingUseCase
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SettingScreenViewModel @Inject constructor(
+    private val saveDailySpendingUseCase: SaveDailySpendingUseCase,
+    getDailySpendingUseCase: GetDailySpendingUseCase
+) : ViewModel() {
+    private val _dailySpend = getDailySpendingUseCase()
+        .stateIn(viewModelScope, SharingStarted.Lazily, null)
+    val dailySpend: StateFlow<Int?> = _dailySpend
+
+    fun saveDailySpending(str: String) {
+        viewModelScope.launch {
+            var amount = 0
+            if (str.length < 5) {
+                if (str.all { it.isDigit() } && str.isNotEmpty()) {
+                    amount = str.toInt()
+                }
+            }
+            saveDailySpendingUseCase(amount)
+        }
+    }
+}
